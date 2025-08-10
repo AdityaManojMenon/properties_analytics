f638ce26e4e9584458aa8bd8e06e986999442903
Adi M <am@Adis-MacBook-Air-3.local>
2025-08-10 18:47:57 -0400

    Finished the scraping scripts for both sales and rental for all the US cities in the list now will have to do the rest of the international cities

---
+++ scraper/scraper/spiders/scrape_sf/sf_sales.py
@@ -0,0 +1,46 @@
+import scrapy
+import json
+import time 
+
+class RedfinSFSpider(scrapy.Spider):
+    name = "SF_sales"
+    allowed_domains = ["redfin.com"]
+
+    #got this from inspecting the network tab (Fetch/XHR) in the browser
+    api_url = "https://www.redfin.com/stingray/api/gis?al=1&clustering_threshold=350&country_code=US&ep=true&isBrokerage=true&lpp=20&market=newyork&mpt=99&num_homes=350&ord=redfin-recommended-asc&page_number=1&poly=-74.16006%2040.34763%2C-73.7316%2040.34763%2C-73.7316%2041.27165%2C-74.16006%2041.27165%2C-74.16006%2040.34763&region_id=30749&region_type=6&school_types=1,2,3&sf=1,2,3,5,6,7&start=0&status=9&uipt=1,2,3,4,5,6,7,8&v=8&zoomLevel=10"
+
+    custom_settings = {
+        "FEEDS": {"chicago_sales.json": {"format": "json", "overwrite": True}},
+        "DEFAULT_REQUEST_HEADERS": {
+            "User-Agent": "Mozilla/5.0",
+            "Referer": "https://www.redfin.com/city/29470/IL/Chicago",
+        },
+    }
+
+    def start_requests(self):
+        yield scrapy.Request(url=self.api_url, callback=self.parse_json)
+
+    def parse_json(self, response):
+        text = response.text
+        if text.startswith("{}&&"):
+            text = text[4:]  # Remove "{}&&" prefix
+
+        data = json.loads(text)
+        homes = data.get("payload", {}).get("homes", [])
+
+        for home in homes:
+            yield {
+                "address": f"{home.get('streetLine', {}).get('value', '')}, {home.get('city')}, {home.get('state')} {home.get('zip')}",
+                "price": home.get("price", {}).get("value"),
+                "beds": home.get("beds"),
+                "baths": home.get("baths"),
+                "sqft": home.get("sqFt", {}).get("value"),
+                "url": f"https://www.redfin.com{home.get('url')}",
+                "neighborhood": home.get("location", {}).get("value"),
+                "lat": home.get("latLong", {}).get("value", {}).get("latitude"),
+                "lng": home.get("latLong", {}).get("value", {}).get("longitude"),
+            }
+
+            # Add 4-second delay per listing
+            self.logger.info("Sleeping for 4 seconds between listings...")
+            time.sleep(4)
