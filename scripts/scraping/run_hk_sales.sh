b8b6ffe1378ebd9f4af7f8bacae36accd68ef8b4
Adi M <am@Adis-MacBook-Air-3.local>
2025-09-06 11:55:31 -0400

    Adjusted the DAG and scraped all of hongkonghomes.com

--- scripts/scraping/run_hk_sales.sh
+++ scripts/scraping/run_hk_sales.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-source .venv/bin/activate
+source ~/Desktop/properties_analytics/.venv/bin/activate
 
 export GOOGLE_CLOUD_PROJECT=property-investment-468818
 export BIGQUERY_DATASET=scrapy_data
 export BIGQUERY_TABLE=HK_listings_sales
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/am/Desktop/gcp/property-investment-468818-74ea0ce1c5ab.json"
 
-cd scraper
+cd ~/Desktop/properties_analytics/scraper
+
 scrapy crawl hong_kong_sales -s JOBDIR=crawls/hk_sales -s DELTAFETCH_ENABLED=True
